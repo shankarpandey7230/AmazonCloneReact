fa53a3411eb2b02e1a41a2d41d4d83326ca0100c
shankarpandey7230 <shankarpandey7230@gmail.com>
2023-05-11 21:33:30 +1000

    Home pages

---
+++ src/Home.jsx
@@ -0,0 +1,15 @@
+import "./Home.css";
+
+const Home = () => {
+  return (
+  <div>
+    <div className="home">
+        <div className="home_container">
+            <img src="" alt="" className="home_image" />
+
+        </div>
+    </div>
+  </div>)
+};
+
+export default Home;

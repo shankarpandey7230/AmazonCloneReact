2800a6af7203273fb046be1b8a626060c6982144
shankarpandey7230 <shankarpandey7230@gmail.com>
2023-05-13 01:16:00 +1000

    passing the react props through products

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,23 +1,23 @@
 import "./Product.css";
 
-const Product = () => {
+const Product = ({ id, title, price, rating, image }) => {
   return (
     <div className="product">
       <div className="product_info">
-        <p>
-          Bennet Mystic 15.6 inch Laptop shoulder Messenger Sling office Bag,
-          Water Repellent Fabric for Men and Women (Blue)
-        </p>
+        <p>{title}</p>
         <p className="product_price">
           <small>$</small>
-          <strong>30</strong>
+          <strong>{price}</strong>
         </p>
-        <div className="product_rating">⭐️⭐️⭐️</div>
+        <div className="product_rating">
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>⭐️</p>
+            ))}
+        </div>
       </div>
-      <img
-        src="https://images-na.ssl-images-amazon.com/images/I/71mEsHyzSCL._SL1000_.jpg"
-        alt="product-image"
-      />
+      <img src={image} alt="product-image" />
       <button>Add to Basket</button>
     </div>
   );
